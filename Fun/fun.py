366ca05c75cd4bca43ef3c2fcde4df0aa86eba11
Martin Fixman <martinfixman@gmail.com>
2024-03-28 13:46:00 +0000

    Summing cross-entropy loss and other improvements.

--- Fun/fun.py
+++ Fun/fun.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import wandb
 from wandb import Artifact
 
-from torch import tensor
+from torch import tensor, Tensor
 from torch.utils.data import DataLoader
 from torch.optim import Adam, Optimizer
 
@@ -32,7 +32,7 @@ class Encoder(nn.Module):
             batch_first = True,
         )
 
-    def forward(self, input : tensor) -> tuple[tensor, tensor]:
+    def forward(self, input : Tensor) -> tuple[Tensor, Tensor]:
         x = self.embedding(input)
         x = self.dropout(x)
         output, (hidden, cell) = self.lstm(x)
@@ -45,14 +45,14 @@ class Decoder(nn.Module):
         # self.relu = nn.ReLU()
         self.lstm = nn.LSTM(input_size = 256 // quotient, hidden_size = 128 // quotient, batch_first = True)
         # self.out = nn.Linear(128 // quotient, 128 // quotient)
-        self.out2 = nn.Linear(128 // quotient, len_vocab)
+        self.out = nn.Linear(128 // quotient, len_vocab)
 
-    def forward(self, input : tensor, hidden : tensor):
+    def forward(self, input : Tensor, hidden : Tensor):
         x = self.embedding(input)
         x, (h, c) = self.lstm(x, hidden)
         x = self.out(x)
         # x = self.relu(x)
-        x = self.out2(x)
+        # x = self.out(x)
         return x, (h, c)
 
 class Runner:
@@ -65,23 +65,24 @@ class Runner:
 
     def __init__(self, n, batch_size, quotient):
         self.load_data(n = n, batch_size = batch_size)
+        self.quotient = quotient
         self.encoder = Encoder(len(self.vocab), quotient).to(device)
         self.decoder = Decoder(len(self.vocab), quotient).to(device)
         self.optimiser = Adam(list(self.encoder.parameters()) + list(self.decoder.parameters()), lr = 1e-3, weight_decay = 1e-3)
-        self.criterion = nn.CrossEntropyLoss().to(device)
+        self.criterion = nn.CrossEntropyLoss(reduction = 'sum', ignore_index = self.vocab[pad]).to(device)
 
         wandb.watch(self.encoder, log = 'all', log_freq = 100)
         wandb.watch(self.decoder, log = 'all', log_freq = 100)
 
     def load_data(self, n = None, batch_size = 256) -> DataLoader:
         print('Loading data', file = sys.stderr)
-        self.vocab = pickle.load(open('vocab.pickle', 'rb'))
+        self.vocab = pickle.load(open('pickles/vocab.pickle', 'rb'))
 
-        train_text = pickle.load(open('train_text.pickle', 'rb'))[:n]
-        train_high = pickle.load(open('train_high.pickle', 'rb'))[:n]
+        train_text = pickle.load(open('pickles/train_text.pickle', 'rb'))[:n]
+        train_high = pickle.load(open('pickles/train_high.pickle', 'rb'))[:n]
 
-        val_text = pickle.load(open('val_text.pickle', 'rb'))[:n]
-        val_high = pickle.load(open('val_high.pickle', 'rb'))[:n]
+        val_text = pickle.load(open('pickles/val_text.pickle', 'rb'))[:n]
+        val_high = pickle.load(open('pickles/val_high.pickle', 'rb'))[:n]
 
         self.text_len = max(max(len(x) for x in train_text), max(len(x) for x in val_text))
         self.high_len = max(max(len(x) for x in train_high), max(len(x) for x in val_high))
@@ -96,43 +97,28 @@ class Runner:
 
         print('Loaded data', file = sys.stderr)
 
-    def run_part(self, text : tensor, high : tensor):
-        encoder_output, (encoder_hidden, encoder_cell) = self.encoder(text)
+    def run_part(self, text : Tensor, high : Tensor):
+        _, hidden = self.encoder(text)
 
         assert all(high[:, 0] == self.vocab[sos])
-        decoder_input = high[:, 0].unsqueeze(1)
-        decoder_hidden = (encoder_hidden, encoder_cell)
+        input = high[:, 0].unsqueeze(1)
 
         loss = tensor(0.).to(device)
-        text_len = text.size(1)
-        high_len = high.size(1)
-        for t in range(1, high_len):
-            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
-            topv, topi = decoder_output.topk(1)
-            decoder_input = topi.squeeze(-1).detach()
+        for t in range(1, self.high_len):
+            output, hidden = self.decoder(input, hidden)
+            _, topi = decoder_output.topk(1)
+            input = topi.squeeze(-1).detach()
 
             loss += self.criterion(decoder_output.squeeze(1), high[:, t])
 
         return loss
 
-    def train(self, text : tensor, high : tensor):
+    def train(self, text : Tensor, high : Tensor):
         self.optimiser.zero_grad()
         loss = self.run_part(text, high)
         loss.backward()
         self.optimiser.step()
         return loss
-
-    def log_models(self):
-        self.log(self.encoder, 'encoder')
-        self.log(self.decoder, 'decoder')
-
-    @staticmethod
-    def log(model, name):
-        torch.save(model.state_dict(), f'{name}.pth')
-
-        artifact = Artifact(f'{name}-weights', type = 'model')
-        artifact.add_file(f'{name}.pth')
-        wandb.log_artifact(artifact)
 
     def run_epoch(self, loader, training):
         loss = 0
@@ -147,6 +133,16 @@ class Runner:
 
         return loss / len(loader.dataset)
 
+    def log_models(self):
+        torch.save(self.encoder.state_dict(), 'encoder.pth')
+        torch.save(self.decoder.state_dict(), 'decoder.pth')
+
+        artifact = Artifact(f'weights', type = 'model', metadata = {'quotient': self.quotient})
+        artifact.add_file('encoder.pth')
+        artifact.add_file('decoder.pth')
+
+        wandb.log_artifact(artifact)
+
 def main():
     config = dict(
         n = 10000,
@@ -159,7 +155,7 @@ def main():
     runner = Runner(n = config['n'], batch_size = config['batch_size'], quotient = config['quotient'])
 
     epochs = config['epochs']
-    last_val_loss = float('inf')
+    best_val_loss = float('inf')
     for e in range(1, epochs):
         print(f'Training epoch {e}', file = sys.stderr)
         train_loss = runner.run_epoch(runner.loader, training = True)
@@ -171,9 +167,10 @@ def main():
         print(f'Epoch {e}: train loss = {train_loss}, val loss = {val_loss}', file = sys.stderr)
         wandb.log({'Epoch': e, 'Train Loss': train_loss, 'Val Loss': val_loss})
 
-        if val_loss < last_val_loss:
+        if val_loss < best_val_loss:
             print('Best loss found!', file = sys.stderr)
             runner.log_models()
+            best_val_loss = val_loss
 
 if __name__ == '__main__':
     main()
