52d0c47f8eeca3fba45acd33df5eee04b4f6f47d
Martin Fixman <martinfixman@gmail.com>
2024-05-15 10:53:36 +0100

    aabati entity.

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import wandb
 class Logger:
     def __init__(self, experiment_name, logger_name='logger', project='inm70x', config = None):
         logger_name = f'{logger_name}-{experiment_name}'
-        logger = wandb.init(project=project, name=logger_name, config = config)
+        logger = wandb.init(entity = 'aabati', project=project, name=logger_name, config = config)
         self.logger = logger
         return
 
